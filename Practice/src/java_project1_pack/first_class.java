ba4ec25afcd441860ca0b84fe6a3f626f9556dbf
Yang <bellehee98@naver.com>
2022-07-05 12:20:42 +0900

    test

---
+++ Practice/src/java_project1_pack/first_class.java
@@ -0,0 +1,88 @@
+package java_project1_pack;
+
+import java.util.Scanner;
+
+public class first_class {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+//		int a = 0, b;
+//		
+//		System.out.print("a = ");
+//		a = sc.nextInt();
+//		System.out.print("b = ");
+//		b = sc.nextInt();
+//
+//		System.out.println("a + b = " + (a + b));
+//		System.out.println("a - b = " + (a - b));
+//		System.out.println("a * b = " + (a * b));
+//		System.out.println("a / b = " + (double) a / b);
+//
+//		float result = (float) a / (float) b;
+//		System.out.println(result);
+//
+//		float result2 = Math.round(result); // Math.round() 반올림
+//		System.out.println("result2 is " + result2);
+//================================================================================
+		String id = null;
+		String password = null;
+//
+//		// ID와 Password를 String 타입으로 입력
+//		System.out.print("ID : ");
+//		id = sc.nextLine();
+//		System.out.print("password : ");
+//		password = sc.nextLine();
+
+//		// 입력받은 ID가 admin이면 관리자 출력
+//		if (id.equals("admin")) {
+//			System.out.println("관리자.");
+//		}
+//		// 입력받은 Password가 123##이면 OK 출력
+//		if (password.equals("123##")) {
+//			System.out.println("OK");
+//		}
+		// 둘 다 만족하면 로그인되었습니다 출력
+//		if (id.equals("admin") && password.equals("123##")) {
+//			System.out.println("로그인되었습니다.");
+//		} else {
+//			System.out.println("다시 시도하십시오.");
+//		}  // Release Code, Product Code 출시하는 코드
+//	===================================================================================	
+//		if (id.equals("admin")) {
+//			if (password.equals("123##")) {
+//				System.out.println("관리자 OK");
+//			} else {
+//				System.out.println("비밀번호 오류");
+//			}
+//		} else {
+//			System.out.println("아이디 오류");
+//		}
+//	===========================================================================	
+//		int i = 1;
+//		while (true) {
+//
+//			System.out.print("password : ");
+//			password = sc.nextLine();
+//
+//			if (password.equals("123##")) {
+//				System.out.println("관리자 OK");
+//				break;
+//			} else {
+//				System.out.println("다시 입력하시오.");
+//
+//			}
+
+//			if (i == 3) {
+//				System.out.println();
+//				System.out.println("**입력횟수 초과**");
+//				break;
+//			}
+//
+//			i++;
+//	}
+
+
+	}
+
+}
